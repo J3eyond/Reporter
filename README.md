8f510e72ebaaf5e30d43dcc99aed239d2e1bf138
thisnotdefault <thisnotdefault@gmail.com>
2022-11-02 21:13:57 +0300

    add Reporter, README.md, req

---
+++ README.md
@@ -0,0 +1,31 @@
+<h1>Reporter</h1>
+<h2>Description</h2>
+Simple script send mail with attachments.
+</br>
+<hr>
+<h2>Install</h2>
+<br>
+<p><i>Clone repository:</i></p>
+<pre>git clone https://github.com/J3eyond/Reporter.git</pre>
+
+<p><i>Enter the folder:</i></p>
+<pre>cd Reporter</pre>
+
+<p><i>Install requirements:</i></p>
+<pre>pip install -r requirements.txt</pre>
+
+<p><i>Create .env file and set lines:</i></p>
+<pre>
+SMTP_SERVER='Smtp server'
+PORT='Port number'
+EMAIL='Email account'
+LOGIN='Email login'
+PASSWORD='Account password'
+SUBJECT='Email subject'
+RECEIVER='Email receiver'
+MESSAGE='Text for email message'
+FOLDER='Your path folder'
+</pre>
+<h2>Requirements</h2>
+<hr>
+<li><b>Python 3.8 +</b></li>
---
+++ reporter.py
@@ -0,0 +1,58 @@
+import mimetypes
+import os
+import smtplib
+import ssl
+from email.message import EmailMessage
+
+from dotenv import load_dotenv
+
+
+class Reporter:
+
+    load_dotenv()
+
+    def __init__(self):
+        self.msg = EmailMessage()
+        self.msg["Subject"] = os.getenv("SUBJECT")
+        self.msg["From"] = os.getenv("EMAIL")
+        self.msg["To"] = os.getenv("RECEIVER")
+        self.msg.set_content(os.getenv("MESSAGE"))
+        self.smtp_server = os.getenv("SMTP_SERVER")
+        self.port = os.getenv("PORT")
+        self.login = os.getenv("LOGIN")
+        self.password = os.getenv("PASSWORD")
+        self.files = [
+            file
+            for file in os.listdir(os.getenv("REPORT_FOLDER"))
+            if os.path.isfile(os.path.join(os.getenv("REPORT_FOLDER"), file))
+        ]
+        self.context = ssl._create_unverified_context()
+
+    def attach_files(self):
+        for file in self.files:
+            mime_type, _ = mimetypes.guess_type(file)
+            mime_type, mime_subtype = mime_type.split("/")
+            with open(f"{os.getenv('REPORT_FOLDER')}{file}", "rb") as attachment:
+                self.msg.add_attachment(
+                    attachment.read(),
+                    maintype=mime_type,
+                    subtype=mime_subtype,
+                    filename=file,
+                )
+
+    def send_email(self):
+        with smtplib.SMTP(self.smtp_server, self.port) as server:
+            server.starttls(context=self.context)
+            server.login(self.login, self.password)
+            server.send_message(self.msg)
+
+
+if __name__ == "__main__":
+    reporter = Reporter()
+    try:
+        reporter.attach_files()
+        reporter.send_email()
+        print("[+] Email success sended.")
+    except (Exception) as e:
+        print(e)
+        print("[-] Email not sended.")
